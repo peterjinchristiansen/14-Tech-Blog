228a0f8a20182c5c1c881afc2a22401beec03f8d
peter <peterjinchristiansen@gmail.com>
2021-10-23 05:45:33 -0600

    nvm

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -32,37 +32,41 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/dashboard', async (req, res) => {
-    let allPosts = []
-    let allComments = []
-    const findPosts = await Post.findAll(
-        { where: { postCreator: req.session.username }},
-        { order: [['updatedAt', 'ASC']] }
-    )
-    const findComments = await Comment.findAll({ order: [['updatedAt', 'ASC']] })
-
-    findPosts.forEach(post => {
-        allPosts.push(post.dataValues)
-    })
-    findComments.forEach(comment => {
-        allComments.push(comment.dataValues)
-    })
-    let allPostsModified = []
-    allPosts.forEach(post => {
-        let thisPostsComments = []
-        for(i = 0; i < allComments.length; i++) {
-            if(post.id === allComments[i].postParent) {
-                thisPostsComments.push(allComments[i])
+    if(req.session.isLoggedIn) {
+        let allPosts = []
+        let allComments = []
+        const findPosts = await Post.findAll(
+            { where: { postCreator: req.session.username }},
+            { order: [['updatedAt', 'ASC']] }
+        )
+        const findComments = await Comment.findAll({ order: [['updatedAt', 'ASC']] })
+    
+        findPosts.forEach(post => {
+            allPosts.push(post.dataValues)
+        })
+        findComments.forEach(comment => {
+            allComments.push(comment.dataValues)
+        })
+        let allPostsModified = []
+        allPosts.forEach(post => {
+            let thisPostsComments = []
+            for(i = 0; i < allComments.length; i++) {
+                if(post.id === allComments[i].postParent) {
+                    thisPostsComments.push(allComments[i])
+                }
             }
-        }
-        post['comments'] = thisPostsComments
-        allPostsModified.push(post)
-    })
-    console.log(req.session.username)
-    res.render('dashboard', {
-        loginStatus: req.session.isLoggedIn,
-        allPosts: allPostsModified,
-        username: req.session.username
-    })
+            post['comments'] = thisPostsComments
+            allPostsModified.push(post)
+        })
+        console.log(req.session.username)
+        res.render('dashboard', {
+            loginStatus: req.session.isLoggedIn,
+            allPosts: allPostsModified,
+            username: req.session.username
+        })
+    } else {
+        res.redirect('/')
+    }
 });
 
 router.get('/login', (req, res) => {
